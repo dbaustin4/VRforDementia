df529016a243b1f58aaf4f1e6ee0b15b43d650c6
Devin Buanahagi <90633513+dbaustin4@users.noreply.github.com>
2025-11-20 15:18:47 +0100

    Scene Transition

---
+++ Assets/HandToStayLoad.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HandStayToLoad : MonoBehaviour
+{
+    public float waitTime = 5.0f;
+    public Renderer sphereRenderer;
+    public Color neutralColor = Color.white;
+    public Color chargedColor = Color.blue;
+
+    public bool isCounting = false;
+    private float timer = 0f;
+
+    void Start()
+    {
+        sphereRenderer.material.color = neutralColor;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "RightPalm" || other.gameObject.tag == "LeftPalm")
+        {
+            if (isCounting == false)
+            {
+                isCounting = true;
+                StartCoroutine(ChargeAndLoad());
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "RightPalm" || other.gameObject.tag == "LeftPalm")
+        {
+            isCounting = false;
+            timer = 0f;
+            sphereRenderer.material.color = neutralColor;
+            StopAllCoroutines();
+        }
+    }
+
+    IEnumerator ChargeAndLoad()
+    {
+        timer = 0f;
+
+        while(timer < waitTime)
+        {
+            timer += Time.deltaTime;
+
+            float t = timer / waitTime;
+            sphereRenderer.material.color = Color.Lerp(neutralColor, chargedColor, t);
+
+            yield return null;
+
+            if (!isCounting)
+                yield break;
+        }
+
+            int currentScene = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(currentScene + 1);
+    }
+
+}
